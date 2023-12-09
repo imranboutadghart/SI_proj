6e5890ba7d3af750eb3b682e6882e92aca90c294
imbo <imranboutadghart1@gmail.com>
2023-12-08 19:36:18 -0800

    phpUpdates

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+include("database.php");
 class product
 {
 	public $id = null;
@@ -15,73 +16,7 @@ class product
 		return $this;
 	}
 }
-
-class node
-{
-	public $data = null;
-	public $next = null;
-
-	function __construct($new_data) {
-		$this->data = $new_data;
-		$this->next = null;
-	}
-	function get_node()
-	{
-		return $this->data;
-	}
-}
-class linked_list
-{
-	private $head;
-	private $last;
-	function __construct() {
-		$this->head = null;
-		$this->last = null;
-	}
-	function add_to_list($data)
-	{
-		if($this->head == null)
-		{
-			$this->head = new Node($data);
-			$this->last = $this->head;
-		}
-		else {
-			$this->last->next = new Node($data);
-		}
-	}
-	function delete_node_with_id($id)
-	{
-		if ($this->head == null)
-		{
-			return;
-		}
-		while($this->head && $this->head->data->id == $id)
-		{
-			$this->head = $this->head->next;
-		}
-		if ($this->head == null)
-		{
-			return;
-		}
-		$prev = $this->head;
-		while($prev->next)
-		{
-			if ($prev->next->data->id == id) {
-				$prev->next = $prev->next->next;
-			}
-		}
-	}
-
-}
-
 	$product = new product(1, "", "test", 70);
-	echo'
-	<div class="product" id='.$product->id.'>
-		<img src="pizza.png" class="image" alt="logo">
-		<div class="description">'. $product->description .'</div>
-		<span class="price">'. $product->price .'</span>
-		<button class="add-to-cart">add to cart</button>
-	</div>'
 ?>
 
 
@@ -107,60 +42,56 @@ class linked_list
         </nav>
 	</header>
 	<div id="products">
-		<div class="product" id="1">
+	<?php
+		echo'
+		<div class="product" id='.$product->id.'>
 			<img src="pizza.png" class="image" alt="logo">
-			<div class="description">Pizza</div>
-			<span class="price">50</span>
+			<div class="description">'. $product->description .'</div>
+			<span class="price">'. $product->price .'</span>
 			<button class="add-to-cart">add to cart</button>
-		</div>
-		<div class="product" id="2">
+		</div>'
+		?><?php
+		echo'
+		<div class="product" id='.$product->id.'>
 			<img src="pizza.png" class="image" alt="logo">
-			<div class="description">Pizza</div>
-			<span class="price">50</span>
+			<div class="description">'. $product->description .'</div>
+			<span class="price">'. $product->price .'</span>
 			<button class="add-to-cart">add to cart</button>
-		</div>
-		<div class="product" id="3">
+		</div>'
+		?><?php
+		echo'
+		<div class="product" id='.$product->id.'>
 			<img src="pizza.png" class="image" alt="logo">
-			<div class="description">Pizza</div>
-			<span class="price">50</span>
+			<div class="description">'. $product->description .'</div>
+			<span class="price">'. $product->price .'</span>
 			<button class="add-to-cart">add to cart</button>
-		</div>
-		<div class="product" id="4">
+		</div>'
+		?><?php
+		echo'
+		<div class="product" id='.$product->id.'>
 			<img src="pizza.png" class="image" alt="logo">
-			<div class="description">Pizza</div>
-			<span class="price">50</span>
+			<div class="description">'. $product->description .'</div>
+			<span class="price">'. $product->price .'</span>
 			<button class="add-to-cart">add to cart</button>
-		</div>
-		<div class="product" id="5">
+		</div>'
+		?><?php
+		echo'
+		<div class="product" id='.$product->id.'>
 			<img src="pizza.png" class="image" alt="logo">
-			<div class="description">Pizza</div>
-			<span class="price">50</span>
+			<div class="description">'. $product->description .'</div>
+			<span class="price">'. $product->price .'</span>
 			<button class="add-to-cart">add to cart</button>
-		</div>
-		<div class="product" id="6">
+		</div>'
+		?>
+		<?php
+		echo'
+		<div class="product" id='.$product->id.'>
 			<img src="pizza.png" class="image" alt="logo">
-			<div class="description">Pizza</div>
-			<span class="price">50</span>
+			<div class="description">'. $product->description .'</div>
+			<span class="price">'. $product->price .'</span>
 			<button class="add-to-cart">add to cart</button>
-		</div>
-		<div class="product" id="7">
-			<img src="pizza.png" class="image" alt="logo">
-			<div class="description">Pizza</div>
-			<span class="price">50</span>
-			<button class="add-to-cart">add to cart</button>
-		</div>
-		<div class="product" id="8">
-			<img src="pizza.png" class="image" alt="logo">
-			<div class="description">Pizza</div>
-			<span class="price">50</span>
-			<button class="add-to-cart">add to cart</button>
-		</div>
-		<div class="product" id="9">
-			<img src="pizza.png" class="image" alt="logo">
-			<div class="description">Pizza</div>
-			<span class="price">50</span>
-			<button class="add-to-cart">add to cart</button>
-		</div>
+		</div>'
+		?>
 		<!-- Add more products as needed -->
 	</div>
 	<div id="cart">    <!-- cart items drop here-->    <p>total<span id="total">0</span></p></div>
